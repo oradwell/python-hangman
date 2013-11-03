9c2a20ccd3ed320d80117929dc929cea986fa61a
Omer Karadagli <ufokaradagli@gmail.com>
2013-11-03 20:44:04 +0000

    Ignore empty string while guessing

--- hangman.py
+++ hangman.py
@@ -17,18 +17,17 @@ for char in phrase:
         chars_in_phrase.add(char)
 
 max_guess = 9
-cur_guess = 0
 wrong_guess = 0
 
 
-
-# While wrong guesses hasn't reached max guesses
+# While wrong guesses hasn't reached max guesses 
+# and not all the characters are guessed
 while wrong_guess < max_guess and chars_in_phrase != correct_guesses:
     # Show number of guesses used out of allowed guesses
     print str(wrong_guess) + "/" + str(max_guess)
     # Print the phrase
     for char in phrase:
-        # Show word separators as is
+        # Show word separators and guessed chars as is
         if char == " " or char == "-" or char in correct_guesses:
             print(char),
         else:
@@ -39,19 +38,22 @@ while wrong_guess < max_guess and chars_in_phrase != correct_guesses:
 
     inchar = raw_input("Enter character: ")
 
-    if len(inchar) > 1:
-        inchar = inchar[0]
+    if len(inchar) < 1:
+        continue
+
+    inchar = inchar[0]
     print "You entered: %s" % inchar
 
     # Check if the character is in the phrase
     if inchar in chars_in_phrase:
         print "correct!"
         correct_guesses.add(inchar)
-    else:
-        # If incorrect - increment wrong guesses
+    elif inchar not in incorrect_guesses:
         print "incorrect!"
         wrong_guess += 1
         incorrect_guesses.add(inchar)
+    else:
+        print "incorrect!"
 
     print "Correct guesses: %s" % correct_guesses
     print "Incorrect guesses: %s" % incorrect_guesses

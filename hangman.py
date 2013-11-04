993dec5d3884e38eee4fb6eeb900cdbcf4d9a10d
Omer Karadagli <ufokaradagli@gmail.com>
2013-11-04 20:18:36 +0000

    Case insensitive guessing, main loop added

--- hangman.py
+++ hangman.py
@@ -2,65 +2,63 @@ print "=================="
 print "= Python Hangman ="
 print "=================="
 
+phrases = ["python hangman", "awesome"]
 # START main loop
-
-# Need to get this from wordlist
-phrase = "python hangman"
-
-# START inner loop
-# get unique characters into a set
-incorrect_guesses = set()
-correct_guesses = set()
-chars_in_phrase = set()
-for char in phrase:
-    if char != " " and char != "-":
-        chars_in_phrase.add(char)
-
-max_guess = 9
-wrong_guess = 0
-
-
-# While wrong guesses hasn't reached max guesses 
-# and not all the characters are guessed
-while wrong_guess < max_guess and chars_in_phrase != correct_guesses:
-    # Show number of guesses used out of allowed guesses
-    print str(wrong_guess) + "/" + str(max_guess)
-    # Print the phrase
+# Should be a random phrase from wordlist instead
+for phrase in phrases:
+    # get unique characters into a set
+    incorrect_guesses = set()
+    correct_guesses = set()
+    chars_in_phrase = set()
     for char in phrase:
-        # Show word separators and guessed chars as is
-        if char == " " or char == "-" or char in correct_guesses:
-            print(char),
+        if char != " " and char != "-":
+            chars_in_phrase.add(char.lower())
+
+    max_guess = 9
+    wrong_guess = 0
+
+    # START inner loop
+    # While wrong guesses hasn't reached max guesses 
+    # and not all the characters are guessed
+    while wrong_guess < max_guess and chars_in_phrase != correct_guesses:
+        # Show number of guesses used out of allowed guesses
+        print str(wrong_guess) + "/" + str(max_guess)
+        # Print the phrase
+        for char in phrase:
+            # Show word separators and guessed chars as is
+            if char == " " or char == "-" or char.lower() in correct_guesses:
+                print(char),
+            else:
+                print("_"),
+
+        # New line
+        print("")
+
+        inchar = raw_input("Enter character: ")
+
+        if len(inchar) < 1:
+            continue
+
+        inchar = inchar[0].lower()
+        print "You entered: %s" % inchar
+
+        # Check if the character is in the phrase
+        if inchar in chars_in_phrase:
+            print "correct!"
+            correct_guesses.add(inchar)
+        elif inchar not in incorrect_guesses:
+            print "incorrect!"
+            wrong_guess += 1
+            incorrect_guesses.add(inchar)
         else:
-            print("_"),
+            print "incorrect!"
 
-    # New line
-    print("")
+        print "Correct guesses: %s" % correct_guesses
+        print "Incorrect guesses: %s" % incorrect_guesses
+        print "Chars in phrase: %s" % chars_in_phrase
+        
+        # END inner loop
 
-    inchar = raw_input("Enter character: ")
+    print "Phrase: %s" % phrase
 
-    if len(inchar) < 1:
-        continue
-
-    inchar = inchar[0]
-    print "You entered: %s" % inchar
-
-    # Check if the character is in the phrase
-    if inchar in chars_in_phrase:
-        print "correct!"
-        correct_guesses.add(inchar)
-    elif inchar not in incorrect_guesses:
-        print "incorrect!"
-        wrong_guess += 1
-        incorrect_guesses.add(inchar)
-    else:
-        print "incorrect!"
-
-    print "Correct guesses: %s" % correct_guesses
-    print "Incorrect guesses: %s" % incorrect_guesses
-    print "Chars in phrase: %s" % chars_in_phrase
-    
-    # END inner loop
-
-print "Phrase: %s" % phrase
-
-# END main loop
+    # END main loop
